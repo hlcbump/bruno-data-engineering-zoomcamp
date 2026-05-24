d10168154e689fa9276347e1069d1ec188e8e95d
Bruno Vinicius Fedora <brunohubbe@gmail.com>
2026-05-24 11:21:04 -0300

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+
+print('arguments', sys.argv)
+
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"day": [1, 2], "numb_passeng": [3, 4]})
+df['month'] = month
+print(df.head())
+
+df.to_parquet(f'output_{month}.parquet')
+
+print(f'Hello Pipeline, month: {month}')
